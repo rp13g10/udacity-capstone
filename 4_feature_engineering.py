a77f8ce5db100f4bf7008bfb28e6b1006e519f07
rp13g10 <37337032+rp13g10@users.noreply.github.com>
2020-06-12 15:10:10 +0100

    Feature Generation
    Merged dataset generated, pending feature scaling & one-hot encoding

--- 4_feature_engineering.py
+++ 4_feature_engineering.py
@@ -11,6 +11,7 @@ import plotly.express as px
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as ssf
 from pyspark.sql import window as ssw
+from pyspark.sql import types as sst
 import pyspark
 
 # Import from module starting with a number
@@ -32,23 +33,11 @@ if script_env == 'local':
 # Feature Engineering                                                                              #
 ####################################################################################################
 
-# Create target variable ###########################################################################
-
-# Get churned customers, without bringing user IDs out of spark
-# churned = data_cleaned.where(data_cleaned['churnFlag'] == 1).select('userId').distinct()
-# churned = churned.withColumn(
-#     #pylint: disable=no-member
-#     'userChurnFlag', ssf.lit(1))
-# data_cleaned = data_cleaned.join(churned, on='userId', how='left')
-# data_cleaned = data_cleaned.fillna(0, subset=['userChurnFlag'])
-# data_cleaned = data_cleaned.persist()
-
 # # Trigger evaluation of the datset up to this point
-# sample = data_cleaned.limit(1000).toPandas()
 data_cleaned = data_cleaned.orderBy(
     'userID', 'ts', ascending=True
 ).persist()
-
+data_sample = data_cleaned.limit(1000).toPandas()
 
 # Static variables (no time dependency) ############################################################
 
@@ -69,41 +58,196 @@ static_vars = static_vars.drop('lastTs', 'registration')
 
 static_sample = static_vars.limit(1000).toPandas()
 
-r"""
-One row per userId
-Account age (max time since registration)
-Gender
-State
-All following features aggregated over 2-3 different date ranges, to be determined
-    No. of systems used
-    Modal system
-    404 error encountered
-    No. distinct artists listened to
-    No. songs listened to
-    No. distinct songs listened to
-    No. distinct sessions
-    Mean items in session
-    Max items in session
-    Stdev items in session
-    Mean time spent listening to each song
-    Stdev time spent listening to each song
-    Page - Add Friend
-    Page - Thumbs up / No. songs
-    Page - Thumbs down / No. songs
-    Page - Help
-    Page - Settings
-    Level @ window start
-    Level @ window end
-    No. users with same surname & same location
-    % of total plays accounted for by songs listened to by the  user
-        how popular is the music they're listening to?
-    Mean no. sessions per day
-"""
-
 # Dynamic variables (evaluated over a time window) #################################################
+
+# Modal system -------------------------------------------------------------------------------------
+
+def get_modal_system(data_in, name):
+    sys_counts = data_in.groupby('userId', 'platform').agg(
+        ssf.countDistinct('sessionId').alias('noSessions'))
+
+    # No built-in mode function, need to manually rank using window functions
+    # Using row_number rather than rank to avoid ties
+    usr_window = ssw.Window.partitionBy(
+        sys_counts['userId']
+    ).orderBy(
+        sys_counts['noSessions'].desc()
+    )
+    sys_counts = sys_counts.withColumn(
+        #pylint: disable=no-member
+        'platformRank', ssf.row_number().over(usr_window)
+    )
+
+    # Take first ranked platform for each 
+    sys_modal = sys_counts.where(
+        sys_counts['platformRank'] == 1
+    ).select(
+        'userId', f'platform'
+    )
+
+    sys_modal = sys_modal.withColumnRenamed('platform', f'platform{name}')
+
+    return sys_modal
+
+# Simple aggregates --------------------------------------------------------------------------------
+
+def get_simple_aggregates(data_in, name):
+
+    flagged = data_in.withColumn(
+        'httpError',
+        ssf.when(data_in['status']==404, 1).otherwise(0)
+    )
+    dynamic_vars = flagged.groupby(
+        'userId'
+    ).agg(
+        #pylint: disable=no-member
+        ssf.count('platform').alias(f'noPlatforms{name}'),
+        ssf.count('userAgent').alias(f'noSystems{name}'),
+        ssf.sum('httpError').alias(f'httpErrors{name}'),
+        ssf.countDistinct('artist').alias(f'noArtists{name}'),
+        ssf.countDistinct('songCleaned').alias(f'noSongs{name}'),
+        # Count excludes nulls, so this should be equivalent to counting
+        # number of nextSong pages
+        ssf.count('songCleaned').alias(f'noPlays{name}'),
+        ssf.first('level').alias(f'levelStart{name}'),
+        ssf.last('level').alias(f'levelEnd{name}')
+    )
+
+    return dynamic_vars
+
+
+# Session stats ------------------------------------------------------------------------------------
+
+def get_session_stats(data_in, name):
+    # Limit to song plays
+    session_vars = data_in.filter(data_in['page'] == 'NextSong')
+
+    # Get play time for each song
+    session_window = ssw.Window.partitionBy(
+        session_vars['userId'], session_vars['sessionId']
+    ).orderBy(
+        'ts'
+    )
+    session_vars = session_vars.withColumn(
+        'nextPlayStart',
+        ssf.lead(session_vars['ts']).over(session_window)
+    )
+
+    session_vars = session_vars.withColumn(
+        'playTime',
+        session_vars['nextPlayStart'] - session_vars['ts']
+    )
+
+    # Get play time for each session
+    session_vars = session_vars.groupby(
+        'userId', 'sessionId'
+    ).agg(
+        #pylint: disable=no-member
+        ssf.sum('playTime').alias('sessionLength')
+    )
+
+    # Get play time stats for each user
+    session_vars = session_vars.groupby(
+        'userId'
+    ).agg(
+        #pylint: disable=no-member
+        ssf.mean('sessionLength').alias(f'lengthMean{name}'),
+        ssf.stddev('sessionLength').alias(f'lengthStd{name}'),
+        ssf.sum('sessionLength').alias(f'lengthSum{name}')
+    )
+
+    return session_vars
+
+
+# Popularity score ---------------------------------------------------------------------------------
+
+def get_popularity_scores(data_in, name):
+    # For each song, how much do they contribute to the total number of plays?
+    song_data = data_in.filter(data_in['page'] == 'NextSong')
+    song_totals = song_data.groupby('songCleaned').agg(
+        #pylint: disable=no-member
+        ssf.count('ts').alias('songTotal')
+    )
+    overall_total = song_totals.agg(
+        #pylint: disable=no-member
+        ssf.sum('songTotal').alias('overallTotal')
+    ).collect()[0]['overallTotal']
+
+    song_totals = song_totals.withColumn(
+        'overallPerc',
+        song_totals['songTotal'] / overall_total
+    ).select(
+        'songCleaned', 'overallPerc'
+    )
+
+    # For each user, how popular are the songs they're playing?
+    popularity = song_data.select('userId', 'songCleaned')
+
+    # Make a clone of popularity, bug in pyspark causes errors if this isn't done
+    # https://stackoverflow.com/questions/45713290/how-to-resolve-the-analysisexception-resolved-attributes-in-spark
+    popularity = spark.createDataFrame(popularity.rdd, popularity.schema)
+
+    popularity = popularity.join(
+        song_totals,
+        popularity['songCleaned']==song_totals['songCleaned'],
+        how='left')
+    popularity = popularity.groupby('userId').agg(
+        #pylint: disable=no-member
+        ssf.sum('overallPerc').alias(f'popularityScore{name}')
+    )
+
+    return popularity
+
+
+# Clicks through to each page ----------------------------------------------------------------------
+
+def get_page_clicks(data_in, name):
+
+    def format_page(page):
+        if not isinstance(page, str):
+            return None
+        
+        page = page.replace(' ', '')
+        page = f'{page}{name}'
+        return page
+    
+    page_getter = ssf.udf(format_page, sst.StringType())
+
+    page_clicks = data_in.withColumn('page', page_getter('page'))
+
+    page_clicks = page_clicks.groupby('userId').pivot('page').count()
+
+    # Ensure consistent column headers regardless of the dataset
+    included_pages = [
+        'About', 'Add Friend', 'Add to Playlist', 'Cancel', 'Downgrade',
+        'Error', 'Help', 'Home', 'Login', 'Logout', 'NextSong', 'Register',
+        'Roll Advert', 'Save Settings', 'Settings', 'Submit Downgrade', 'Submit Registration',
+        'Submit Upgrade', 'Thumbs Down', 'Thumbs Up', 'Upgrade'
+    ]
+
+    included_pages = ['userId'] + [format_page(x) for x in included_pages]
+
+    missing_pages = [x for x in included_pages if x not in page_clicks.columns]
+
+    # Add a column for any missing pages
+    for missing_page in missing_pages:
+        page_clicks = page_clicks.withColumn(
+            #pylint: disable=no-member
+            missing_page,
+            ssf.lit(0)
+        )
+
+    # Only bring through what's in the list
+    page_clicks = page_clicks.select(*included_pages)
+
+    return page_clicks
+
+
+# Generate all required tables ---------------------------------------------------------------------
 
 # Get the max timestamp
 limits = data_cleaned.agg(
+    #pylint: disable=no-member
     ssf.max('ts').alias('maxTs'),
     ssf.min('ts').alias('minTs')
 ).collect()[0]
@@ -119,157 +263,61 @@ start_times = {
     'month': max_ts - month_delta
 }
 
-# outputs = []
-# for name, start_ts in start_times.items():
+outputs = None
+for name, start_ts in start_times.items():
+
+    name = name.title()
+
     # Get subset of data
-    # Calculate aggregate statistics
-    # Scale where appropriate
+    data_subset = data_cleaned.where(
+        data_cleaned['ts'] >= start_ts
+    ).persist()
 
+    modal_system = get_modal_system(data_subset, name)
 
-# Modal system -------------------------------------------------------------------------------------
-sys_counts = data_cleaned.groupby('userId', 'platform').agg(
-    ssf.countDistinct('sessionId').alias('noSessions'))
+    simple_aggregates = get_simple_aggregates(data_subset, name)
 
-# No built-in mode function, need to manually rank using window functions
-# Using row_number rather than rank to avoid ties
-usr_window = ssw.Window.partitionBy(
-    sys_counts['userId']
-).orderBy(
-    sys_counts['noSessions'].desc()
-)
-sys_counts = sys_counts.withColumn(
-    #pylint: disable=no-member
-    'platformRank', ssf.row_number().over(usr_window)
-)
+    session_stats = get_session_stats(data_subset, name)
 
-# Take first ranked platform for each 
-sys_modal = sys_counts.where(
-    sys_counts['platformRank'] == 1
-).select(
-    'userId', 'platform'
-)
+    popularity_scores = get_popularity_scores(data_subset, name)
 
-# Simple aggregates --------------------------------------------------------------------------------
+    page_clicks = get_page_clicks(data_subset, name)
 
-flagged = data_cleaned.withColumn(
-    'httpError',
-    ssf.when(data_cleaned['status']==404, 1).otherwise(0)
-)
-dynamic_vars = flagged.groupby(
-    'userId'
-).agg(
-    #pylint: disable=no-member
-    ssf.count('platform').alias('noPlatforms'),
-    ssf.count('userAgent').alias('noSystems'),
-    ssf.sum('httpError').alias('httpErrors'),
-    ssf.countDistinct('artist').alias('noArtists'),
-    ssf.countDistinct('songCleaned').alias('noSongs'),
-    # Count excludes nulls, so this should be equivalent to counting
-    # number of nextSong pages
-    ssf.count('songCleaned').alias('noPlays'),
-    ssf.first('level').alias('levelStart'),
-    ssf.last('level').alias('levelEnd')
+    merged = modal_system.join(
+        simple_aggregates,
+        on='userId',
+        how='full_outer'
+    ).join(
+        session_stats,
+        on='userId',
+        how='full_outer'
+    ).join(
+        popularity_scores,
+        on='userId',
+        how='full_outer'
+    ).join(
+        page_clicks,
+        on='userId',
+        how='full_outer'
+    ).persist()
+
+    if outputs is None:
+        outputs = merged
+    else:
+        outputs = outputs.join(merged, on='userId', how='full_outer')
+
+merged = static_vars.join(
+    outputs,
+    on='userId',
+    how='full_outer'
 )
 
-
-# Session stats ------------------------------------------------------------------------------------
-
-# Limit to song plays
-session_vars = data_cleaned.filter(data_cleaned['page'] == 'NextSong')
-
-# Get play time for each song
-session_window = ssw.Window.partitionBy(
-    session_vars['userId'], session_vars['sessionId']
-).orderBy(
-    'ts'
-)
-session_vars = session_vars.withColumn(
-    'nextPlayStart',
-    ssf.lead(session_vars['ts']).over(session_window)
-)
-
-session_vars = session_vars.withColumn(
-    'playTime',
-    session_vars['nextPlayStart'] - session_vars['ts']
-)
-
-# Get play time for each session
-session_vars = session_vars.groupby(
-    'userId', 'sessionId'
-).agg(
-    ssf.sum('playTime').alias('sessionLength')
-)
-
-# Get play time stats for each user
-session_vars = session_vars.groupby(
-    'userId'
-).agg(
-    ssf.mean('sessionLength').alias('lengthMean'),
-    ssf.stddev('sessionLength').alias('lengthStd'),
-    ssf.sum('sessionLength').alias('lengthSum')
-)
-
-
-# Popularity score ---------------------------------------------------------------------------------
-
-# For each song, how much do they contribute to the total number of plays?
-song_data = data_cleaned.filter(data_cleaned['page'] == 'NextSong')
-song_totals = song_data.groupby('songCleaned').agg(
-    ssf.count('ts').alias('songTotal')
-)
-overall_total = song_totals.agg(
-    ssf.sum('songTotal').alias('overallTotal')
-).collect()[0]['overallTotal']
-
-song_totals = song_totals.withColumn(
-    'overallPerc',
-    song_totals['songTotal'] / overall_total
-).select(
-    'songCleaned', 'overallPerc'
-)
-
-# For each user, how popular are the songs they're playing?
-popularity = song_data.select('userId', 'songCleaned')
-
-# Make a clone of popularity, bug in pyspark causes errors if this isn't done
-# https://stackoverflow.com/questions/45713290/how-to-resolve-the-analysisexception-resolved-attributes-in-spark
-popularity = spark.createDataFrame(popularity.rdd, popularity.schema)
-
-popularity = popularity.join(
-    song_totals,
-    popularity['songCleaned']==song_totals['songCleaned'],
-    how='left')
-popularity = popularity.groupby('userId').agg(
-    ssf.sum('overallPerc').alias('popularityScore')
-)
-
-
-# Clicks through to each page ----------------------------------------------------------------------
-page_clicks = data_cleaned.groupby('userId').pivot('page').count()
-
-# Ensure consistent column headers regardless of the dataset
-included_pages = [
-    'userId', 'About', 'Add Friend', 'Add to Playlist', 'Cancel', 'Downgrade',
-    'Error', 'Help', 'Home', 'Login', 'Logout', 'NextSong', 'Register',
-    'Roll Advert', 'Save Settings', 'Settings', 'Submit Downgrade', 'Submit Registration',
-    'Submit Upgrade', 'Thumbs Down', 'Thumbs Up', 'Upgrade'
-]
-
-missing_pages = [x for x in included_pages if x not in page_clicks.columns]
-
-# Add a column for any missing pages
-for missing_page in missing_pages:
-    page_clicks = page_clicks.withColumn(
-        missing_page,
-        ssf.lit(0)
-    )
-
-# Only bring through what's in the list, fill in any missing values with 0
-page_clicks = page_clicks.select(*included_pages)
-page_clicks = page_clicks.fillna(0)
-
-
-# Bring everything together ------------------------------------------------------------------------
+platform_cols = [x for x in outputs.columns if 'platform' in x]
+level_cols = [x for x in outputs.columns if 'level' in x]
+merged = merged.fillna('unknown', subset=platform_cols)
+merged = merged.fillna('free', subset=level_cols)
+merged = merged.fillna(0)
+merged = merged.persist()
 
 # Final transformations ############################################################################
 
